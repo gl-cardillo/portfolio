b4503aa6043c7156d6ce0936763193b86e42194b
luca-cardi <giovanniluca.cardillo@gmail.com>
2024-01-14 12:53:54 +0000

    fix bug

--- components/Projects.js
+++ components/Projects.js
@@ -51,168 +51,116 @@ export function Projects() {
         >
           <span className="bg-white dark:bg-black py-5">Projects</span>
         </h2>
-        <motion.div
-          initial={{ x: "-100vw" }}
-          animate={inView && { x: 0 }}
-          transition={{ duration: 1.5 }}
-          className="w-[90%] md:w-[70%] xl:w-[60%] p-2.5 rounded-lg mt-12 z-10 shadow-[0_0_20px_1px_#9f9f9f] self-start"
-        >
-          <h3 className="text-3xl mb-5 font-montserrat font-semibold">
-            The Odinbook
-          </h3>
-          <Slider {...settings}>
-            {slideOdinbook.map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                alt=""
-                className="rounded-md w-full"
-              />
-            ))}
-          </Slider>
-          <p className="m-5 font-roboto font-medium">
-            The Odinbook is a project from The Odin project curriculum, the goal
-            is to build a full-stack application similar to Facebook, where the
-            user can create an account, make post etc.
-          </p>
-          <p className="flex gap-1 2xs:gap-2 justify-center items-center font-bold text-2xl">
-            <span className=" text-sm 2xs:text-base 2xs:font-montserrat sm:text-lg">
-              Made with:
-            </span>
-            <FaReact />
-            <FaNodeJs />
-            <DiMongodb />
-            <SiPassport />
-            <SiJest />
-            <FaCss3Alt />{" "}
-          </p>
-          <div className="flex justify-center gap-5 mt-5">
-            <a
-              href="https://the-odinbook.herokuapp.com/"
-              target="_blank"
-              rel="noreferrer"
-              className="p-2 rounded-lg text-white dark:text-black bg-neutral-900 dark:bg-white text-xl border  hover:scale-110"
-            >
-              <BiLinkExternal />
-            </a>
-            <a
-              href="https://github.com/gl-cardillo/the-odinbook"
-              target="_blank"
-              rel="noreferrer"
-              className="p-2 rounded-lg text-white dark:text-black bg-neutral-900 dark:bg-white text-xl border  hover:scale-110"
-            >
-              <FaCode />
-            </a>
-          </div>
-        </motion.div>
-        <p ref={ref2}></p>
-        <motion.div
-          initial={{ x: "-100vw" }}
-          animate={inView2 && { x: 0 }}
-          transition={{ duration: 1.25 }}
-          className="w-[90%] md:w-[70%] xl:w-[60%] p-2.5 rounded-lg mt-12 z-10 shadow-[0_0_20px_1px_#9f9f9f] self-end"
-        >
-          <h3 className="text-3xl mb-5 font-montserrat font-semibold">
-            Amazon clone
-          </h3>
-          <Slider {...settings}>
-            {slideAmazon.map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                className="rounded-md w-full"
-                alt=""
-              />
-            ))}
-          </Slider>
-          <p className="m-5 font-roboto font-medium">
-            This project is an Amazon clone, the user can browse throw
-            categories and products, sign up, log in, log out, delete the
-            account, add reviews with ratings, add products to the cart and
-            remove them
-          </p>
-          <p className="flex gap-1 2xs:gap-2 justify-center items-center font-bold text-2xl">
-            <span className="text-sm 2xs:text-base 2xs:font-montserrat sm:text-lg">
-              {" "}
-              Made with:
-            </span>
-            <SiNextdotjs />
-            <DiMongodb />
-            <SiTailwindcss />
-          </p>
-          <div className="flex justify-center gap-5 mt-5">
-            <a
-              href="https://amazon-clone-phi-green.vercel.app/"
-              target="_blank"
-              rel="noreferrer"
-              className="p-2 rounded-lg text-white dark:text-black bg-neutral-900 dark:bg-white text-xl border hover:scale-110"
-            >
-              <BiLinkExternal />
-            </a>
-            <a
-              href="https://github.com/gl-cardillo/amazon-clone"
-              target="_blank"
-              rel="noreferrer"
-              className="p-2 rounded-lg text-white dark:text-black bg-neutral-900 dark:bg-white text-xl border hover:scale-110"
-            >
-              <FaCode />
-            </a>
-          </div>
-        </motion.div>{" "}
-        <p ref={ref3}></p>
-        <motion.div
-          initial={{ x: "-100vw" }}
-          animate={inView3 && { x: 0 }}
-          transition={{ duration: 1.25 }}
-          className="w-[90%] md:w-[70%] xl:w-[60%] p-2.5 rounded-lg mt-12 z-10 shadow-[0_0_20px_1px_#9f9f9f] self-start"
-        >
-          <h3 className="text-3xl mb-5 font-montserrat font-semibold">
-            Instapets
-          </h3>{" "}
-          <Slider {...settings}>
-            {slideInstapets.map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                className="rounded-md w-full"
-                alt=""
-              />
-            ))}
-          </Slider>
-          <p className="m-5 font-roboto font-medium">
-            Instapets is a project from The Odin project curriculum, the goal is
-            to build a full-stack application similar to Instagram but for pets,
-            where the user can create an account for the pets, post pictures
-            etc.
-          </p>
-          <p className="flex gap-1 2xs:gap-2 justify-center items-center font-bold text-2xl">
-            <span className="text-sm 2xs:text-base 2xs:font-montserrat sm:text-lg">
-              {" "}
-              Made with:
-            </span>
-            <FaReact />
-            <IoLogoFirebase />
-            <FaCss3Alt />
-          </p>
-          <div className="flex justify-center gap-5 mt-5">
-            <a
-              href="https://gl-cardillo.github.io/instapets/#/login"
-              target="_blank"
-              rel="noreferrer"
-              className="p-2 rounded-lg text-white dark:text-black bg-neutral-900 dark:bg-white text-xl border border-black hover:scale-110"
-            >
-              <BiLinkExternal />
-            </a>
-            <a
-              href="https://github.com/gl-cardillo/instapets"
-              target="_blank"
-              rel="noreferrer"
-              className="p-2 rounded-lg text-white dark:text-black bg-neutral-900 dark:bg-white text-xl border border-black  hover:scale-110"
-            >
-              <FaCode />
-            </a>
-          </div>
-        </motion.div>
+        <div className="flex xl:flex-row flex-col justify-between">
+          <p ref={ref2}></p>
+          <motion.div
+            initial={{ x: "-150vw" }}
+            animate={inView2 && { x: 0 }}
+            transition={{ duration: 1.25 }}
+            className="w-[90%] md:w-[70%] xl:w-[45%] h-[600px] p-2.5 rounded-lg mt-12 z-10 shadow-[0_0_20px_1px_#9f9f9f] self-end"
+          >
+            <h3 className="text-3xl mb-5 font-montserrat font-semibold">
+              Amazon clone
+            </h3>
+            <Slider {...settings}>
+              {slideAmazon.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  className="rounded-md w-full max-h-[260px]"
+                  alt=""
+                />
+              ))}
+            </Slider>
+            <p className="m-5 font-roboto font-medium">
+              This project is an Amazon clone, the user can browse throw
+              categories and products, sign up, log in, log out, delete the
+              account, add reviews with ratings, add products to the cart and
+              remove them
+            </p>
+            <p className="flex gap-1 2xs:gap-2 justify-center items-center font-bold text-2xl">
+              <span className="text-sm 2xs:text-base 2xs:font-montserrat sm:text-lg">
+                {" "}
+                Made with:
+              </span>
+              <SiNextdotjs />
+              <DiMongodb />
+              <SiTailwindcss />
+            </p>
+            <div className="flex justify-center gap-5 mt-5">
+              <a
+                href="https://amazon-clone-phi-green.vercel.app/"
+                target="_blank"
+                rel="noreferrer"
+                className="p-2 rounded-lg text-white dark:text-black bg-neutral-900 dark:bg-white text-xl border hover:scale-110"
+              >
+                <BiLinkExternal />
+              </a>
+              <a
+                href="https://github.com/gl-cardillo/amazon-clone"
+                target="_blank"
+                rel="noreferrer"
+                className="p-2 rounded-lg text-white dark:text-black bg-neutral-900 dark:bg-white text-xl border hover:scale-110"
+              >
+                <FaCode />
+              </a>
+            </div>
+          </motion.div>{" "}
+          <p ref={ref3}></p>
+          <motion.div
+            initial={{ x: "-100vw" }}
+            animate={inView3 && { x: 0 }}
+            transition={{ duration: 1.25 }}
+            className="w-[90%] md:w-[70%] xl:w-[45%] h-[600px] p-2.5 rounded-lg mt-12 z-10 shadow-[0_0_20px_1px_#9f9f9f] self-start"
+          >
+            <h3 className="text-3xl mb-5 font-montserrat font-semibold">
+              Instapets
+            </h3>{" "}
+            <Slider {...settings}>
+              {slideInstapets.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  className="rounded-md w-full max-h-[260px]"
+                  alt=""
+                />
+              ))}
+            </Slider>
+            <p className="m-5 font-roboto font-medium">
+              Instapets is a project from The Odin project curriculum, the goal
+              is to build a full-stack application similar to Instagram but for
+              pets, where the user can create an account for the pets, post
+              pictures etc.
+            </p>
+            <p className="flex gap-1 2xs:gap-2 justify-center items-center font-bold text-2xl">
+              <span className="text-sm 2xs:text-base 2xs:font-montserrat sm:text-lg">
+                {" "}
+                Made with:
+              </span>
+              <FaReact />
+              <IoLogoFirebase />
+              <FaCss3Alt />
+            </p>
+            <div className="flex justify-center gap-5 mt-5">
+              <a
+                href="https://gl-cardillo.github.io/instapets/#/login"
+                target="_blank"
+                rel="noreferrer"
+                className="p-2 rounded-lg text-white dark:text-black bg-neutral-900 dark:bg-white text-xl border border-black hover:scale-110"
+              >
+                <BiLinkExternal />
+              </a>
+              <a
+                href="https://github.com/gl-cardillo/instapets"
+                target="_blank"
+                rel="noreferrer"
+                className="p-2 rounded-lg text-white dark:text-black bg-neutral-900 dark:bg-white text-xl border border-black  hover:scale-110"
+              >
+                <FaCode />
+              </a>
+            </div>
+          </motion.div>
+        </div>
       </div>
       <div className="border-b border-slate-400 max-w-[500px] self-center py-7 mx-auto  flex justify-center text-xl">
         <a
